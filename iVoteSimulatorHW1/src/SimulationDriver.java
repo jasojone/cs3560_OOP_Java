4ccbcece388b07a1d4019a1b77f67bff8555fa87
Jason Jones <jasojone@gmail.com>
2022-09-28 21:32:10 -0700

    added votingservice logic

--- iVoteSimulatorHW1/src/SimulationDriver.java
+++ iVoteSimulatorHW1/src/SimulationDriver.java
@@ -3,7 +3,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
-
 import QuestionTypes.MultipleChoice;
 import QuestionTypes.Questions;
 import QuestionTypes.SingleChoice;
@@ -17,35 +16,32 @@ public class SimulationDriver {
     public static void main(String[] args) throws Exception {
         Questions q = new SingleChoice();
         Questions mcq = new MultipleChoice();
-        
+        voteSimLogic(q);
+    }
+    
+    public static void voteSimLogic(Questions q){
         q.setQuestions("What is a book made of");
         q.setChoices(Arrays.asList("Paper", "Metal", "Plastic", "Foam"));
-        List<String> choices = q.getChoices();
+        //List<String> choices = q.getChoices();
         q.setAnswers(Arrays.asList("Paper"));
-        List<String> answer = q.getAnswers();
+        //List<String> answer = q.getAnswers();
         // creates an array of size 20 * the size of the student object in the heap
         Student[] s = new Student[20];
         Random rand = new Random();
-        //for(int i = 0; i < s.length; i++){
-        //     s[i];
-        // System.out.println("Student ID: " + s[i].getUniqueId() + " Choice: " + s[i].getAnswersUI());
-        // }
-        Map<String, String> student_answers = new HashMap<String, String>();
+        //Map<String, String> student_answers = new HashMap<String, String>();
         for (Student student : s) {
             int randChoice = rand.nextInt(q.getChoices().size());
             List<String> choiceUI = Arrays.asList(q.getChoices().get(randChoice));
-            //System.out.println(choiceUI);
              student = new Student(); 
              student.setAnswersUI(choiceUI);
-             String studentID = student.getUniqueId().toString();
-             String studentAns = student.getAnswersUI().toString();
-             student_answers.put(studentID,studentAns);
-             System.out.println(student_answers.toString());
+             //String studentID = student.getUniqueId().toString();
+             //String studentAns = student.getAnswersUI().toString();
+             //student_answers.put(student.getUniqueId().toString(),student.getAnswersUI().toString());
+             //System.out.println(student_answers.toString());
              //System.out.println("Student ID: " + student.getUniqueId() + " Choice: " + student.getAnswersUI());
         }
         VotingService v = new VotingService(q, s);
-        v.checkAnswers(student_answers, choices, answer);
-
-
+        System.out.println("Question: " + q.getQuestions());
+        v.checkAnswers(Student[]);
     }
 }
